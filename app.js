2dbbcf99a8e0e48abfe7f9f6e66f99557392d8f9
ATG-5222 <A01275222@tec.mx>
2023-03-27 12:48:15 -0600

    Configurar paquetes iniciales

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
