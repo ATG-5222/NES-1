7ce623610ad1b920cbb48e072abf3ec9582c5350
ATG-5222 <A01275222@tec.mx>
2023-03-27 12:51:27 -0600

    Creación de carpetas necesarias

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ const port = 3000;
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(session({
+    secret: 'qweasdzxc',
+    resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
+    saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
+}));
+
+app.use(express.static(path.join(__dirname, 'publics')));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
