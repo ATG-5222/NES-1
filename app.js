87d826e10eb5f207066e1e5fad48ff3daa8d0be5
ATG-5222 <A01275222@tec.mx>
2023-03-27 23:19:28 -0600

    Eliminar dependencias no usadas

--- app.js
+++ app.js
@@ -2,20 +2,11 @@ const express = require('express');
 
 const path = require('path');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(cookieParser());
-
-app.use(session({
-    secret: 'qweasdzxc',
-    resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
-    saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
-}));
 
 app.use(express.static(path.join(__dirname, 'publics')));
 app.set('view engine', 'ejs');
