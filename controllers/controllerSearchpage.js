1d06d40d2d67bf2450f91130907e5738206eb289
ATG-5222 <A01275222@tec.mx>
2023-04-03 14:14:47 -0600

    DataTable funcional

--- controllers/controllerSearchpage.js
+++ controllers/controllerSearchpage.js
@@ -1,15 +1,18 @@
 const Inmueble = require('../models/modelInmueble');
 
-exports.getPrincipalSearchpage = (req,res,next) => {
-    Inmueble.principalSearchPage()
-        .then(([rows, fieldData]) => {
-            res.send(rows);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+
+exports.getSearchpage = (req,res,next) => {
+    console.log("Este es el render basico de la pagina");
+    res.render('searchpage');
 };
 
+exports.getPrincipalSearchpage = async(req,res,next) => {
+    console.log("Este es el controller de ajax");
+    inmuebles = await Inmueble.principalSearchPage();
+    res.status(200).json({code:200,code:"Ok",data:inmuebles[0]})
+};
+
+//Modificar esto
 exports.getAjaxSearchpage = (req,res,next) => {
     res.render('searchpage', {
         params: req.params.valor ? req.params.valor : ' ',
--- routes/routeSearchpage.js
+++ routes/routeSearchpage.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 const searchpageController = require('../controllers/controllerSearchpage');
 
-router.get('/:valor', searchpageController.getAjaxSearchpage);
-router.get('/', searchpageController.getPrincipalSearchpage);
+router.get('/busqueda_datos/:valor', searchpageController.getAjaxSearchpage);
+router.get('/busqueda_datos', searchpageController.getPrincipalSearchpage);
+router.get('/', searchpageController.getSearchpage);
 
 module.exports = router;
