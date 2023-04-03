9eea4d21d9c0fc8e982e94d9e2babeb89ee18bb9
ATG-5222 <A01275222@tec.mx>
2023-04-03 14:16:10 -0600

    Eliminar comentarios

--- controllers/controllerSearchpage.js
+++ controllers/controllerSearchpage.js
@@ -1,13 +1,10 @@
 const Inmueble = require('../models/modelInmueble');
 
-
 exports.getSearchpage = (req,res,next) => {
-    console.log("Este es el render basico de la pagina");
     res.render('searchpage');
 };
 
 exports.getPrincipalSearchpage = async(req,res,next) => {
-    console.log("Este es el controller de ajax");
     inmuebles = await Inmueble.principalSearchPage();
     res.status(200).json({code:200,code:"Ok",data:inmuebles[0]})
 };
